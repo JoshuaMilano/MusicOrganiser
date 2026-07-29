607b0d8720afb9526d76825aa441763998e5724c
Joshua Milano <milanoj925@gmail.com>
2026-07-29 04:18:30 +0100

    Changed how text to use a helper class for colouring.

--- helpers.py
+++ helpers.py
@@ -11,3 +11,37 @@ def get_primary_artist(artist_string):
     parts = re.split(r'(?i)\s*(?:;|\bfeat\.?\b|\bft\.?\b)', str(artist_string))
 
     return parts[0].strip()
+
+
+
+
+# Format
+class formatText:
+    """Allows text to be formatted with different colours"""
+
+    ERROR = '\033[31m' # RED
+    SUCCESS = '\033[32m' # GREEN
+    ALERT = '\033[33m' # YELLOW
+    INFO = '\033[34m' # BLUE
+    RESET = '\033[0m' # RESET TEXT
+
+    # Method definitions
+    @classmethod
+    def error(cls, message):
+        """Formats the terminal text as RED"""
+        return f'{cls.ERROR}{message}{cls.RESET}'
+
+    @classmethod
+    def success(cls, message):
+        """Formats the terminal text as GREEN"""
+        return f'{cls.SUCCESS}{message}{cls.RESET}'
+
+    @classmethod
+    def alert(cls, message):
+        """Formats the terminal text as YELLOW"""
+        return f'{cls.ALERT}{message}{cls.RESET}'
+
+    @classmethod
+    def info(cls, message):
+        """Formats the terminal text as BLUE"""
+        return f'{cls.INFO}{message}{cls.RESET}'
--- main.py
+++ main.py
@@ -3,15 +3,7 @@ import time
 import mutagen
 import shutil
 
-from helpers import sanitize_name, get_primary_artist
-
-
-
-ERROR = '\033[31m' # RED
-SUCCESS = '\033[32m' # GREEN
-ALERT = '\033[33m' # YELLOW
-INFO = '\033[34m' # BLUE
-RESET = '\033[0m' # Plain TExt
+from helpers import formatText, sanitize_name, get_primary_artist
 
 
 def organise_directory(source_path, target_path):
@@ -35,7 +27,7 @@ def organise_directory(source_path, target_path):
     # Set to store the PATH of the artists we've found.
     processed_artists = set()
 
-    print(f'Found {INFO}{len(audio_files)} {RESET}audio files. Reading Metadata...\n')
+    print(f'Found {formatText.info(len(audio_files))} audio files. Reading Metadata...\n')
     print('-' * 40)
 
     for index, file_path in enumerate(audio_files, 1):
@@ -47,8 +39,7 @@ def organise_directory(source_path, target_path):
                 
 
             if audio is None:
-                print(f'[{file_path.name}] -> No readable tags found!')
-                print('-' * 40)
+                print(formatText.info(f'[{file_path.name}] -> No readable tags found!'))
                 continue
 
             raw_artist = audio.get('artist', ['Unknown Artist'])[0]
@@ -87,7 +78,7 @@ def organise_directory(source_path, target_path):
                     artist_img_target = artist_folder / f'artist{artist_img_source.suffix.lower()}'
                     if not artist_img_target.exists():
                         shutil.copy2(artist_img_source, artist_img_target)
-                        print(f'{SUCCESS}COPIED ARTIST PIC: {safe_album_artist}')
+                        print(formatText.success(f'COPIED ARTIST PICTURE: {safe_album_artist}'))
 
                 processed_artists.add(artist_folder)
 
@@ -102,28 +93,26 @@ def organise_directory(source_path, target_path):
 
                     if not cover_target.exists():
                         shutil.copy2(cover_source, cover_target)
-                        print(f'{SUCCESS}COPIED ALBUM ART: {safe_album}')
+                        print(formatText.success(f'COPIED ALBUM ART: {safe_album}'))
 
                 processed_albums.add(new_folder)
 
             if not new_file_path.exists():
                 shutil.copy2(file_path, new_file_path)
-                print(f'{SUCCESS}[{index}/{len(audio_files)}] COPIED: {safe_artist} -> {safe_title}')
+                print(formatText.success(f'[{index}/{len(audio_files)}] COPIED: {safe_artist} -> {safe_album} -> {safe_title}'))
             else:
-                print(f'{INFO}[{index}/{len(audio_files)}] SKIPPED (Already exists): {safe_title}')
+                print(formatText.info(f'[{index}/{len(audio_files)}] SKIPPED (Already exists): {safe_title}'))
 
         except Exception as e:
-            print(f'{ERROR}-' * 40)
-            print(f'{ERROR}[{file_path.name}] -> CRASHED: {str(e)}')
-            print(f'{ERROR}-' * 40)
+            print(formatText.error(f'[{file_path.name}] -> CRASHED: {str(e)}'))
 
     # Return the data we want to display :)
     return len(audio_files), len(processed_albums), len(processed_artists)
 
 if __name__ == '__main__':
-    target_folder = input('Enter the path to the folder to be organised:\n')
+    target_folder = input(formatText.alert('\nEnter the path to the folder to be organised:\n'))
     target_folder = target_folder.strip('"').strip("'")
-    target_path = input('Enter the path to the folder to send the music:\n')
+    target_path = input(formatText.alert('\nEnter the path to the folder to send the music:\n'))
 
     # Remove accidental quotations from input.
     target_path = target_path.strip('"').strip("'")
@@ -138,8 +127,8 @@ if __name__ == '__main__':
     minutes = int(elapsed_time // 60)
     seconds = elapsed_time % 60
 
-    print(f'\n{RESET}Music Organised')
-    print(f'\nSorted {INFO}{total_artists} {RESET}Artists')
-    print(f'\nSorted {INFO}{total_albums} {RESET}Albums')
-    print(f'\nSorted {INFO}{total_songs} {RESET}Songs')
-    print(f'\nTotal execution time: {INFO}{minutes} minutes and {seconds:.2f} seconds{RESET}')
+    print(formatText.alert('\nMusic Organised'))
+    print(f'\nSorted {formatText.info(total_artists)} Artists')
+    print(f'\nSorted {formatText.info(total_albums)} Albums')
+    print(f'\nSorted {formatText.info(total_songs)} Songs')
+    print(f'\nTotal execution time: {formatText.info(f'{minutes} minutes and {seconds:.2f} seconds')}')
